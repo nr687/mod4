85d07475ecb9240f4ecb30d33c85bc557a91b7e5
nr687 <nr687@scarletmail.rutgers.edu>
2025-05-07 21:54:48 -0400

    Initial commit

---
+++ Assets/_Labs/99-SupportFiles/UIReturnToMainMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIReturnToMainMenu : MonoBehaviour
+{
+    //todo: dtermine if I want to set up different return points from this prefab
+    private GameObject returnMenu;
+    //public GameObject fpsController;
+
+
+    public void Awake()
+    {
+       
+        //returnMenu.SetActive(false);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Mouse0)))
+        {
+            GameObject.Find("ReturnToMenu-Equals").SetActive(false);
+            
+
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Equals)))
+        {
+            ReturnToMainMenu();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mod4-WorldBuilding-2023
+ 
